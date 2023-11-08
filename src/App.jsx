f112edd9826e0e2c4a9983f25c0163ec5dd47ce0
Felix <moxiemeyoh@gmail.com>
2023-11-08 17:49:40 +0300

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX}  ${styles.flexCenter}`}>
-        <div className={`styles.boxWidth`}>
+        <div className={`${styles.boxWidth}`}>
           <Navbar/>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import { close, logo, menu } from '../assets';
+import { navLinks } from './../constants';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='w-full  flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="hoobank"  className='w-[124px] h-[32px]'/>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {
+          navLinks.map((link) => (
+            <li
+            key={link.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white `}
+            >
+              <a href={`#${link.id}`}></a>
+              { link.title}
+            </li>
+          ))
+        }
+      </ul>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
