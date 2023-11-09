62e84740f0cb5550fb1c41d0f599ddce29273b5e
Felix <moxiemeyoh@gmail.com>
2023-11-09 09:34:45 +0300

    comleted stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { stats } from '../constants';
+import styles from '../style';
 
-function Starts() {
-  return (
-    <div>Starts</div>
-  )
-}
+const  Starts = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((item) => (
+      <div key={item.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+        <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+          {item.value}
+        </h4>
+        <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>
+          {item.title}
+        </p>
+      </div>
+    ))}
+  </section>
+)
 
-export default Starts
+export default Starts;
