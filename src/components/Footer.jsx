1b7c47294b2cd654e317833f6698e40df06849cb
Felix <moxiemeyoh@gmail.com>
2023-11-10 13:00:00 +0300

    ....

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => ( 
   <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8  w-full`}>
       <div className='flex-1 flex-col justify-start mr-10'>
         <img src={logo} alt="hoobank"  className='w-[266px] h-[72px] object-contain'/>
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
